95d7f1c17326d4d8c09a0560d586bf8898186ea0
Emi Ichimura <s1f102000488@iniad.org>
2020-12-04 16:27:59 +0900

    TODOlist/urls.
    add TODOlist/urls.

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog import views as list_views
+from TODOlist import views as list_views
+from django.urls import path, include
+from TODOlist import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('')
+    path('TODOlist/', include('TODOlist.urls'))
 ]
